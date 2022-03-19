147f7874045d37f38f729fc1b6cffa5c2fec5c1c
D.Kallas <69872252+Dkalcos@users.noreply.github.com>
2022-03-19 16:19:04 -0300

    Add files via upload
    Projeto estoque ADS Noturno
    Aluno: Davi Kallas

---
+++ pjava/src/com/company/Apolice.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Apolice {
+
+    private String nomeSegurado;
+    private int idade;
+    private float valorPrimo;
+
+
+    public void imprimir(){
+
+
+
+    }
+
+}
